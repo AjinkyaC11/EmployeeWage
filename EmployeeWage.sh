8548f8f7102457218e40986e99680d4f82853211
unknown <chaware.ajinkya59@gmail.com>
2021-01-06 20:11:01 +0530

    2nd Task is done

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -3,11 +3,15 @@
 echo "Welcome to Employeewage"
 
 isPresent=1;
+wagePerHour=20;
+NoOfHour=8;
 checkAttendance=$((RANDOM%2))
 
 if [ $isPresent -eq $checkAttendance ]
 then
        echo "Employee Present"
+       payment=$(($wagePerHour*$NoOfHour));
+       echo "Payment is : " $payment
 else
        echo " Employee absent"
 fi
