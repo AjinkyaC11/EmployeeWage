36879b9da521650ef433eb71e8f8df630383aae4
unknown <chaware.ajinkya59@gmail.com>
2021-01-06 20:05:15 +0530

    1st Task

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,2 +1,13 @@
+#!/bin/bash -x
 
 echo "Welcome to Employeewage"
+
+isPresent=1;
+checkAttendance=$((RANDOM%2))
+
+if [ $isPresent -eq $checkAttendance ]
+then
+       echo "Employee Present"
+else
+       echo " Employee absent"
+fi
