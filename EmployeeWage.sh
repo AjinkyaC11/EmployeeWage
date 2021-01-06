f37ae9c86d2dc76412722a7bb6e72573c0065ad0
unknown <chaware.ajinkya59@gmail.com>
2021-01-06 20:23:23 +0530

    3rd task is done

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -3,15 +3,25 @@
 echo "Welcome to Employeewage"
 
 isPresent=1;
+isPartTime=2;
+RatePerHour=20;
 wagePerHour=20;
+noOfPartTimeHour=4;
 NoOfHour=8;
-checkAttendance=$((RANDOM%2))
+checkAttendance=$((RANDOM%3))
 
 if [ $isPresent -eq $checkAttendance ]
 then
        echo "Employee Present"
        payment=$(($wagePerHour*$NoOfHour));
        echo "Payment is : " $payment
+elif [ $isPartTime -eq $checkAttendance ]
+then
+     echo "Employee is Part Time !"
+     payment=$(($RatePerHour*$noOfPartTimeHour))
+     echo "Payment is : " $payment
+
+
 else
        echo " Employee absent"
 fi
